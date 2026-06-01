c7218728970302fd2d6b5ffe70847e0a03be22d0
Hallyssonals <hallyssonals@gmail.com>
2026-06-01 23:15:11 +0000

    Alteração nas paginas

--- app/experiencia-academica/page.js
+++ app/experiencia-academica/page.js
@@ -12,8 +12,44 @@ export default function ExperienciaAcademica() {
 
         {/* INSIRA SUA FORMAÇÃO AQUI */}
         <p className="mt-4">
-          Curso, faculdade, certificações e estudos.
+         Curso Superior em Sistemas para internet - Universidade catolica de pernambuco
         </p>
+        <p className="mt-4">
+        Atualmente desenvolvendo conhecimentos em:
+
+        </p>
+        <p className="mt-4">
+         • Desenvolvimento Front-End
+        </p>
+        <p className="mt-4">
+        • Desenvolvimento Web com React e Next.js
+
+        </p>
+        <p className="mt-4">
+         • Programação Orientada a Objetos
+        </p>
+        <p className="mt-4">
+        • Estrutura de Dados
+
+        </p>
+        <p className="mt-4">
+        • Banco de Dados
+
+        </p>
+        <p className="mt-4">
+        • Engenharia de Software
+
+        </p>
+        <p className="mt-4">
+        • APIs e Integrações
+
+        </p>
+        <p className="mt-4">
+        • Versionamento de Código com Git e GitHub
+
+        </p>
+        
+
       </section>
     </main>
   );
--- app/sobre/page.js
+++ app/sobre/page.js
@@ -11,11 +11,15 @@ export default function Sobre() {
         </h1>
 
         <p className="mt-4">
-          Aqui você irá escrever informações pessoais e profissionais.
+          Olá! Meu nome é Hallysson Araújo Laurindo Silva.
+          Sou estudante da área de Tecnologia da Informação e atuo profissionalmente com suporte técnico e soluções de comunicação empresarial. Tenho experiência com atendimento ao cliente, implantação de sistemas, plataformas de atendimento omnichannel, WhatsApp Business API e tecnologias VoIP.
+          Atualmente busco aprimorar meus conhecimentos em desenvolvimento web, com foco em React, Next.js e integração de APIs, desenvolvendo projetos acadêmicos e pessoais que contribuem para minha evolução profissional.
+          Tenho grande interesse por tecnologia, acessibilidade digital, desenvolvimento de software e soluções que gerem impacto positivo para usuários e empresas.
         </p>
 
         <h2 className="text-2xl font-bold mt-8">
-          Tecnologias utilizadas
+          Este portfólio foi desenvolvido utilizando as seguintes tecnologias:
+
         </h2>
 
         <ul className="list-disc ml-6 mt-4">
